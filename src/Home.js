712f5b1bd052c16bf80de5e0b90366a5a3693515
Devon Wolfkiel <devonwolf@tutanota.com>
2021-02-25 09:38:23 -0800

    Remove dropdown from homepage, replace with link to add new yarn

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,7 @@ import React, { Component } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 
 export default class Home extends Component {
-	state = {
-		dropdown: ''
-	}
 
-	handleDropdown = e => {
-		this.setState({ dropdown: e.target.value })
-	}
-
-	handleDropdownClick = () => {
-		if (this.state.dropdown) {
-			<Redirect to={`/yarn/${this.state.dropdown}`} />
-		}
-	}
-// NOTE TO SELF: not all ids exist anymore, need to remove hardcoded ids in the homepage dropdown, make these click-throughs from the list page
 	render() {
 		
 		return (
@@ -34,18 +21,7 @@ export default class Home extends Component {
 					</section>
 				
 					<section>
-						<select onChange={this.handleDropdown}>
-							<option value="">--</option>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-							<option value="6">6</option>
-						</select>
-						{ this.state.dropdown
-						? <Link className="dropdown-link" to={`/yarn/${this.state.dropdown}`}>...but you decide to click anyway</Link>
-						: <span>You don't know what these numbers mean...</span>}
+						<Link className="button-link" to="/new">Add to the Yarnstash</Link>
 					</section>
 			
 			</main>
